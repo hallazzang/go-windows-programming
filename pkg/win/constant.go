f907c923ec27f54fedf3a11ef6e776b6ad33b3d9
Hanjun Kim <hallazzang@gmail.com>
2019-03-03 10:17:03 +0000

    update win package

--- pkg/win/constant.go
+++ pkg/win/constant.go
@@ -11,6 +11,10 @@ const (
 	CS_VREDRAW = 0x0001
 )
 
+const (
+	CW_USEDEFAULT = ^0x7fffffff
+)
+
 // LoadCursor constants
 const (
 	IDC_ARROW = 32512
@@ -27,6 +31,17 @@ const (
 	SW_SHOW        = 5
 	SW_SHOWDEFAULT = 10
 	SW_SHOWNORMAL  = 1
+)
+
+// Window styles
+const (
+	WS_CAPTION          = 0x00c00000
+	WS_MAXIMIZEBOX      = 0x00010000
+	WS_MINIMIZEBOX      = 0x00020000
+	WS_OVERLAPPED       = 0x00000000
+	WS_SYSMENU          = 0x00080000
+	WS_THICKFRAME       = 0x00040000
+	WS_OVERLAPPEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX
 )
 
 // WTS API constants
